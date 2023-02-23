135db113da99c525e6017cb7e8fbe4d0ab8b3b63
Daniel Moreira <101654176+Daniel-Moreira26@users.noreply.github.com>
2023-02-23 18:07:11 -0300

    Update ex008.js

--- curso.js/ex008.js
+++ curso.js/ex008.js
@@ -1,11 +1,27 @@
 var agora = new Date()
-var diaSem = agora.getDay
+diaSem = agora.getDay()
 
-switch (diaSem) {
-    case 1:
-        console.log ('Domingo')
+
+switch(diaSem) {
+    case 0:
+        console.log('Domingo')
         break
-    case 2: 
-        console.log ('Segunda')
+    case 1: 
+        console.log('Segunda')
+        break
+    case 2:
+        console.log('terça')
+        break
+    case 3: 
+        console.log('Quarta')
+        break
+    case 4:
+        console.log('quinta')
+        break
+    case 5:
+        console.log('sexta')
+        break
+    case 6:
+        console.log('sabado')
         break
 }
