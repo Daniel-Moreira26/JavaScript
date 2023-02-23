6367098201d87f022aedf1605fa36dca37544322
Daniel Moreira <101654176+Daniel-Moreira26@users.noreply.github.com>
2023-02-23 10:49:13 -0300

    Create ex008.js

---
+++ curso.js/ex008.js
@@ -0,0 +1,11 @@
+var agora = new Date()
+var diaSem = agora.getDay
+
+switch (diaSem) {
+    case 1:
+        console.log ('Domingo')
+        break
+    case 2: 
+        console.log ('Segunda')
+        break
+}
