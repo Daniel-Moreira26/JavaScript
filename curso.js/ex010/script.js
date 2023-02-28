b9f243f0f45ecb116982407563a1d2b9ba7b2e08
Daniel Moreira <101654176+Daniel-Moreira26@users.noreply.github.com>
2023-02-28 18:58:41 -0300

    atualizei meu exercicios

--- curso.js/ex010/script.js
+++ curso.js/ex010/script.js
@@ -10,15 +10,48 @@ function verificar() {
         var fsex = document.getElementsByName('radsex')
         var idade = ano - Number(fano.value)
         var genero = ''
-
+        var img = document.createElement('img')
+        img.setAttribute('id', 'foto')
         if (fsex[0].checked) {
             genero = 'Homem'
+            if (idade >= 0 && idade <= 10) {
+                //criança
+                img.setAttribute('src', 'bebe-homem250.jpg')
+
+            } else if (idade <= 21) {
+                //jovem
+                img.setAttribute('src', 'jovem-homem250.jpg')
+                
+
+            } else if (idade <= 50) {
+                //adulto
+                img.setAttribute('src', 'homem-adulto250.jpg')
+
+            } else {
+                //idoso
+                img.setAttribute('src', 'velho-homem250.jpg')
+
+            }
         } else if (fsex[1].checked) {
             genero = 'Mulher'
+            if (idade >= 0 && idade <= 10) {
+                //criança
+                img.setAttribute('src', 'bebemulher250.jpg')
+            } else if (idade <= 21) {
+                //jovem
+                img.setAttribute('src', 'jovem-mulher250.jpg')
+            } else if (idade <= 50) {
+                //adulto
+                img.setAttribute('src', 'mulher-adulta250.jpg')
+            } else {
+                //idoso
+                img.setAttribute('src', 'velha-mulher250.jpg')
+            }
         }
 
         res.style.textAlign = 'center'
         res.innerHTML = `É um(a) ${genero} de ${idade} anos` 
+        res.appendChild(img)
 
         
     }
