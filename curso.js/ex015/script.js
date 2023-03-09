48543ab6954057a6f498a3062995557e2aa0ba75
Daniel Moreira <101654176+Daniel-Moreira26@users.noreply.github.com>
2023-03-09 16:41:29 -0300

    atualizei

--- curso.js/ex015/script.js
+++ curso.js/ex015/script.js
@@ -1,10 +1,20 @@
-function calcular() {
-    var tabu = document.getElementById('tabuada')
-    var res = document.getElementById('res')
-    var tab = Number(tabu.value)
-
-    if (tabu.value.length == 0 ) {
+function tabuada() {
+    var num = document.getElementById('txtn')
+    var tab = document.getElementById('seltab')
+    
+    if (num.value.length == 0 ) {
         res.innerText = '[INVÁLIDO] Deverá digitar um número!'
         tab = 0
+    } else {
+        var n = Number(num.value)
+        var c = 1
+        tab.innerHTML = '' // para limpar a tabuada antes de ir para outra
+
+        while (c <= 10) {
+        var item = document.createElement('option')
+        item.text = `${n} x ${c} = ${n*c}`
+        tab.appendChild(item)
+        c++
+        }
     }
 }
